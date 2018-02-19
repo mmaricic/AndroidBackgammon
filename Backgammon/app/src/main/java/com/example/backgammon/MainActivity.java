edb0c1700e6fa13959eb662766630f33a0115697
MarijaMaricic <marija190396@gmail.com>
2018-02-19 02:46:56 +0100

    final

--- Backgammon/app/src/main/java/com/example/backgammon/MainActivity.java
+++ Backgammon/app/src/main/java/com/example/backgammon/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
             editor.apply();
         }
 
-       // setStatistic();
+       setStatistic();
     }
 
     private void setStatistic() {
--- Backgammon/app/src/main/java/com/example/backgammon/players/CompPlayer.java
+++ Backgammon/app/src/main/java/com/example/backgammon/players/CompPlayer.java
@@ -4,10 +4,12 @@ import android.media.MediaPlayer;
 import android.os.AsyncTask;
 
 import com.example.backgammon.R;
+import com.example.backgammon.gameLogic.CalculationState;
 import com.example.backgammon.gameLogic.GameLogic;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by Marija on 12.2.2018..
@@ -21,18 +23,18 @@ public class CompPlayer extends Player {
     @Override
     public void play(GameLogic gameLogic) {
 
-      new Movement(gameLogic).execute();
+        new Movement(gameLogic).execute();
     }
 
     @Override
     public void rollDices(final GameLogic gameLogic) {
 
-        new AsyncTask<Void, Void, Void>(){
+        new AsyncTask<Void, Void, Void>() {
 
             @Override
             protected Void doInBackground(Void... voids) {
                 try {
-                    MediaPlayer  mediaPlayer = MediaPlayer.create(gameLogic.getContext(), R.raw.dices);
+                    MediaPlayer mediaPlayer = MediaPlayer.create(gameLogic.getContext(), R.raw.dices);
                     mediaPlayer.setLooping(true);
                     mediaPlayer.start();
                     Thread.sleep(1000);
@@ -52,15 +54,15 @@ public class CompPlayer extends Player {
         }.execute();
     }
 
-    private class Movement extends AsyncTask< Void, int[], Integer>{
+    private class Movement extends AsyncTask<Void, int[], Integer> {
         private GameLogic gameLogic;
 
-        public Movement(GameLogic gameLogic){
+        public Movement(GameLogic gameLogic) {
             this.gameLogic = gameLogic;
         }
 
         @Override
-        protected Integer doInBackground( Void... voids) {
+        protected Integer doInBackground(Void... voids) {
             try {
                 Thread.sleep(1500);
             } catch (InterruptedException e) {
@@ -69,11 +71,23 @@ public class CompPlayer extends Player {
             LinkedHashMap<Integer, ArrayList<Integer>> moves = gameLogic.getPossibleMoves();
             ArrayList<Integer> possibleMoves = null;
             int oldPosition = -1;
-            while((possibleMoves = moves.get(oldPosition)) == null) {
-                oldPosition = (int) (Math.random() * 24);
+            int newPosition = 0;
+            if (state == CalculationState.BearingOffState) {
+               for(Map.Entry<Integer, ArrayList<Integer>> entry: moves.entrySet()){
+                   if(entry.getValue().contains(24)){
+                       oldPosition = entry.getKey();
+                       break;
+                   }
+               }
+                newPosition = possibleMoves.size() - 1;
             }
-            int newPosition = (int) (Math.random()*possibleMoves.size());
-            if(oldPosition == -1)
+            if(oldPosition == -1) {
+                while ((possibleMoves = moves.get(oldPosition)) == null)
+                    oldPosition = (int) (Math.random() * 24);
+                newPosition = (int) (Math.random() * possibleMoves.size());
+            }
+
+            if (oldPosition == -1)
                 newPosition = 0;
             int res[] = new int[2];
             res[0] = oldPosition;
